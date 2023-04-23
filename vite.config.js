c29739d6731d9597d8d1a83f1075db27936264ef
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-04-24 03:40:18 +0530

    backend folder is now outside

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://food-point-backend.onrender.com/'
+      '/api':'https://food-point-backend.onrender.com'
     }
   },
   plugins: [react()],
