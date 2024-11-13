b70ffd55eb2c17d4901d8ef564e2e9eab724f151
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2024-11-13 15:14:43 +0530

    fixed responsice issue on dish items

--- src/components/DishItems/DishItems.jsx
+++ src/components/DishItems/DishItems.jsx
@@ -21,33 +21,28 @@ const DishItems = ({ dishData }) => {
   const [dishes, setDishes] = React.useState(dishesList);
   console.log(dishesList);
   function scrollRight() {
-    document.querySelector('.dishes-container').scrollBy({
-      left: 250,
-      behavior: 'smooth'
-  })
-
+    document.querySelector(".dishes-container").scrollBy({
+      left: 300,
+      behavior: "smooth",
+    });
   }
   function scrollLeft() {
-    document.querySelector('.dishes-container').scrollBy({
-      left: -250,
-      behavior: 'smooth'
-  })
+    document.querySelector(".dishes-container").scrollBy({
+      left: -300,
+      behavior: "smooth",
+    });
   }
   return (
     <div className="dishes">
       <h3>{header ? header : "Whats on your mind ??"}</h3>
       <div className="vh-flex-center">
-        <NavigateBeforeIcon
-          onClick={scrollLeft}
-          />
+        <NavigateBeforeIcon onClick={scrollLeft} className="prev-arrow" />
         <div className="dishes-container">
           {dishes?.map((dish, index) => (
             <DishItem dish={dish} key={dish.id} />
           ))}
         </div>
-        <NavigateNextIcon
-          onClick={scrollRight}
-        />
+        <NavigateNextIcon className="next-arrow" onClick={scrollRight} />
       </div>
     </div>
   );
