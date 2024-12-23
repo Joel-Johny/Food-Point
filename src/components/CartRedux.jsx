41793f781e2259355e462d07eb0580efc7fe6f67
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2024-12-23 19:20:19 +0530

    cart redux image styling, sum of cart items bug fix

--- src/components/CartRedux.jsx
+++ src/components/CartRedux.jsx
@@ -1,74 +1,74 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
+import RestaurantItem from "./RestaurantMenu/RestaurantItem";
 import DeleteCart from "./DeleteCart";
 import { useNavigate } from "react-router-dom";
-const CartRedux=()=>{
-    const navigate=useNavigate()
+const CartRedux = () => {
+  const navigate = useNavigate();
 
-    const deleteAll=()=>{
-        setAlert(true) 
-        
-    }
-    const goHome=()=>{
-        navigate(`/`)
-    }
+  const deleteAll = () => {
+    setAlert(true);
+  };
+  const goHome = () => {
+    navigate(`/`);
+  };
 
-    const goBack=()=>{
-        navigate(`/restaurant/`+r_id)
-    }
-    const [alert,setAlert]=React.useState(false)
-    let sum=0
-    const {items,total,rname,r_id,totalBill}=useSelector((store)=>{
-        return store.cart
-    })
-    const allCartItems=items.map((item,index)=>{
-        return(
-            <CartItem details={item} key={index}/>
-        )
-    })
+  const goBack = () => {
+    navigate(`/restaurant/` + r_id);
+  };
+  const [alert, setAlert] = React.useState(false);
+  const { items, total, rname, r_id, totalBill } = useSelector((store) => {
+    return store.cart;
+  });
+  const allCartItems = items.map((item, index) => {
+    return <RestaurantItem details={item} key={index} />;
+  });
 
-    return (
-        <>
-        <div className={alert ? 'opacity' : 'crContainer'}>
-            
-            <div className="rest-menu">
-                {(total>0)
-                ?(
-                    <>
-                    <h2 className="recommend">Your Cart Items from {rname}</h2>
-                    {allCartItems}
-                    <div className="bill-container">
-                        <big className="bill"> Your total bill amount is <strong> ₹{totalBill}</strong></big>
-                    </div>
-                    <div className="buttons">
-                        <div className="see-rest" onClick={deleteAll}>Empty Cart?</div>
-                        <div className="see-rest" onClick={goBack}>Go back to Restaurant</div>
-                    </div>
-                    
-
-                    </>
-                )
-                :(
-                    <><div className="empty">
-                        <img src='./cart_empty.png' style={{margin:'1rem',padding:`1rem`,height:`20rem`}}/>
-                        <h2>Your Cart is Empty</h2>
-                        <big>You can go to home page to view more restaurants</big>
-                        <div className="see-rest" onClick={goHome}>SEE RESTAURANTS NEAR YOU</div>
-                    </div>
-
-                    </>
-                )
-                }
-            </div>
-
-        </div>
+  return (
+    <>
+      <div className={`h-flex-center ${alert} ? 'opacity' : 'crContainer'`}>
         <div className="rest-menu">
-        {alert && <DeleteCart setter={setAlert}/>}
+          {total > 0 ? (
+            <>
+              <h3 className="recommend">Your Cart Items from {rname}</h3>
+              {allCartItems}
+              <div className="bill-container">
+                <big className="bill">
+                  Your total bill amount is <strong> ₹{totalBill}</strong>
+                </big>
+              </div>
+              <div className="buttons">
+                <div className="see-rest" onClick={deleteAll}>
+                  Empty Cart?
+                </div>
+                <div className="see-rest" onClick={goBack}>
+                  Go back to Restaurant
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="empty">
+                <img
+                  src="./cart_empty.png"
+                  className="cart-empty"
+                />
+                <h4>Your Cart is Empty</h4>
+                <div className="see-rest" onClick={goHome}>
+                  <label>SEE RESTAURANTS NEAR YOU</label>
+                </div>
+              </div>
+            </>
+          )}
         </div>
+      </div>
+      <div className="h-flex-center">
+        {alert && <DeleteCart setter={setAlert} />}
+      </div>
 
-        </>
-    )
-}
+    </>
+  );
+};
 
-export default CartRedux
+export default CartRedux;
--- src/utils/cartSlice.jsx
+++ src/utils/cartSlice.jsx
@@ -1,82 +1,87 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartSlice=createSlice({
-    name:'cart',
+const cartSlice = createSlice({
+  name: "cart",
 
-    initialState:{
-        items:[],//this is the cart item
-        total:0,
-        conflict:false,
-        rname:'',
-        r_id:0,
-        totalBill:0,
+  initialState: {
+    items: [], //this is the cart item
+    total: 0,
+    conflict: false,
+    rname: "",
+    r_id: 0,
+    totalBill: 0,
+  },
+  reducers: {
+    addItem: (state, action) => {
+      // let sum=0
+      if (state.items.length == 0 || state.rname == action.payload.rname) {
+        state.items.push(action.payload);
+        state.rname = action.payload.rname;
+        state.r_id = action.payload.r_id;
+        state.total = state.total + 1;
+        cartSlice.caseReducers.updateBill(state);
+      }
+      // state.items.map((object,index)=>{
+      //     sum=sum+object.count
+      // })
+      // state.total=sum
+      else {
+        state.conflict = true;
+      }
     },
-    reducers:{
-        addItem: (state,action)=>{
-            // let sum=0
-            if (state.items.length==0 || state.rname==action.payload.rname){
-                state.items.push(action.payload)
-                state.rname=action.payload.rname
-                state.r_id=action.payload.r_id
-                state.total=state.total+1
-                cartSlice.caseReducers.updateBill(state)
-            }
-            // state.items.map((object,index)=>{
-            //     sum=sum+object.count
-            // })
-            // state.total=sum
+    incrementItem: (state, action) => {
+      state.items.map((object, index) => {
+        if (object.name == action.payload.name) {
+          object.count = object.count + 1;
+          cartSlice.caseReducers.updateBill(state);
+        }
+      });
 
-            else{
-                state.conflict=true
-            }
+      state.total = state.total + 1;
+    },
 
-        },
-        incrementItem:(state,action)=>{
-            state.items.map((object,index)=>{
-                if(object.name==action.payload.name){
-                    object.count=object.count+1
-                    cartSlice.caseReducers.updateBill(state)
-                }
-            })
+    decrementItem: (state, action) => {
+      state.items.map((object, index) => {
+        if (object.name == action.payload.name) {
+          object.count = object.count - 1;
 
-            state.total=state.total+1
-        },
+          if (object.count == 0) {
+            state.items.splice(index, 1);
+          }
+          cartSlice.caseReducers.updateBill(state);
+        }
+      });
 
-        decrementItem:(state,action)=>{
-            state.items.map((object,index)=>{
-                if(object.name==action.payload.name){
-                    object.count=object.count-1
+      state.total = state.total - 1;
+    },
+    conflictResolve: (state) => {
+      state.conflict = false;
+    },
+    clearCart: (state) => {
+      state.items = [];
+      // he says we cant do the above
+      state.total = 0;
+    },
+    updateBill: (state) => {
+      let sum = 0;
+      state.items.map((object) => {
+        console.log(object);
+        sum =
+          sum +
+          ((object.price ? object.price : object.defaultPrice) / 100) *
+            object.count;
+      });
+      state.totalBill = sum;
+    },
+  },
+});
 
-                if(object.count==0){
-                    state.items.splice(index,1)
-                    }
-                cartSlice.caseReducers.updateBill(state)
-
-                }
-            })
-
-            state.total=state.total-1
-        },
-        conflictResolve:(state)=>{
-            state.conflict=false
-        },
-        clearCart:((state)=>{
-            state.items=[]
-            // he says we cant do the above
-            state.total=0
-        }),
-        updateBill:((state)=>{
-            let sum=0
-
-            state.items.map((object)=>{
-                sum=sum+((object.price/100)*object.count)
-            })
-            state.totalBill=sum
-
-        })
-    }
-
-})
-
-export default cartSlice.reducer
-export const {addItem,incrementItem,decrementItem,clearCart,conflictResolve,updateBill} =cartSlice.actions
+export default cartSlice.reducer;
+export const {
+  addItem,
+  incrementItem,
+  decrementItem,
+  clearCart,
+  conflictResolve,
+  updateBill,
+} = cartSlice.actions;
