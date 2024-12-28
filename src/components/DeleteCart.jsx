493d4a44192aab24b54145769a82aa518b2a7a47
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2024-12-28 18:30:15 +0530

    bug fix on redux redux cart items when emptying cart

--- src/components/DeleteCart.jsx
+++ src/components/DeleteCart.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
+import React from "react";
 import { clearCart } from "../utils/cartSlice";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
+import { useThemeContext } from "../utils/ThemeContext";
 
-const DeleteCart = ({setter}) => {
-const dispatch=useDispatch()
+const DeleteCart = ({ setter }) => {
+  const dispatch = useDispatch();
+  const { theme } = useThemeContext();
 
-const handleYes =()=>{
-
-    dispatch(clearCart())
-    setter(false)
-}
-const handleNo =()=>{
-
-    setter(false)
-}
+  const handleYes = () => {
+    dispatch(clearCart());
+    setter(false);
+  };
+  const handleNo = () => {
+    setter(false);
+  };
   return (
-    <div className="delete">
-        <h2>Delete all items in your cart?</h2>
-        <div className="decision">
-            <div className="no" onClick={handleNo}>NO</div>
-            <div className="yes" onClick={handleYes}>YES</div>
+    <div className={`delete ${theme}`}>
+      <h2>Delete all items in your cart?</h2>
+      <div className="decision">
+        <div className="no" onClick={handleNo}>
+          NO
         </div>
+        <div className="yes" onClick={handleYes}>
+          YES
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DeleteCart
+export default DeleteCart;
